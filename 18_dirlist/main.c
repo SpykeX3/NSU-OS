f2811ea7d7837bbcea23a2e1e7b3d74fe11f940a
SpykeX3 <spykex3@gmail.com>
2019-12-04 14:47:24 +0700

    18 refactor

--- 18_dirlist/main.c
+++ 18_dirlist/main.c
@@ -7,34 +7,45 @@
 #include <time.h>
 #include <stdlib.h>
 
-void printState(struct stat *st) {
-    if (S_ISDIR(st->st_mode)) printf("d");
-    else if (S_ISREG(st->st_mode)) printf("-");
-    else printf("?");
+#define PERM_READ(x) (x%3==2)
+#define PERM_WRITE(x) (x%3==1)
+
+void print_state(struct stat *st) {
+    if (S_ISDIR(st->st_mode)) {
+        printf("d");
+    } else if (S_ISREG(st->st_mode)) {
+        printf("-");
+    } else {
+        printf("?");
+    }
     for (int i = 8; i >= 0; i--) {
         if (st->st_mode & (1 << i)) {
-            if (i % 3 == 2)
+            if (PERM_READ(i)) {
                 printf("r");
-            else if (i % 3 == 1)
+            } else if (PERM_WRITE(i)) {
                 printf("w");
-            else printf("x");
-        } else printf("-");
+            } else {
+                printf("x");
+            }
+        } else {
+            printf("-");
+        }
     }
     printf("   %-5d", st->st_nlink);
 }
 
-void printUser(struct stat *st) {
+void print_user(struct stat *st) {
     struct passwd *password = getpwuid(st->st_uid);
     printf("    %-15s", password->pw_name);
     struct group *group = getgrgid(st->st_gid);
     printf("    %-15s", group->gr_name);
 }
 
-void printName(char *path) {
+void print_name(char *path) {
     char *name = path;
     for (char *start = path; *start != 0; start++) {
         if (*start == '/' && start[1] != 0) {
-            name = start+1;
+            name = start + 1;
         }
     }
     printf("   %s\n", name);
@@ -49,14 +60,17 @@ int main(int argc, char **argv) {
             printf("%s: %s\n", argv[i], strerror(errno));
             continue;
         }
-        printState(&st);
-        printUser(&st);
-        if (S_ISREG(st.st_mode))
+        print_state(&st);
+        print_user(&st);
+        if (S_ISREG(st.st_mode)) {
             printf("  %-6ld", st.st_size);
-        else printf("  %-6c", ' ');
+        } else {
+            printf("  %-6c", ' ');
+        }
         char *time = asctime(localtime(&st.st_mtime));
         time[strlen(time) - 1] = 0;
         printf("  %s", time);
-        printName(argv[i]);
+        print_name(argv[i]);
     }
+    return 0;
 }
